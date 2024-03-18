10628e7774fc2cb7bf6efd3e60981c8a772a42f5
Igor Chesnykh (notebook) <igor.chesnyx@mail.ru>
2024-03-18 09:18:45 +0300

    Create all tasks in 3 contest and add lexion 5

--- solution_on_lexions.py
+++ solution_on_lexions.py
@@ -1,38 +1,53 @@
-def find_min_greater_equal(arr, value, mn_arr, mx_arr):
-    left = mn_arr
-    right = mx_arr
-    if right < value:
-        return -1
-    while right - left > 1:
-        md = (left + right) // 2
-        if md < value:
-            left = md
-        else:
-            right = md
-    return right
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
 
+def height(node):
+    if node is None:
+        return 0
+    return 1 + max(height(node.left), height(node.right))
 
+def is_balanced(node):
+    if node is None:
+        return True
+    lh = height(node.left)
+    rh = height(node.right)
+    if abs(lh - rh) <= 1 and is_balanced(node.left) and is_balanced(node.right):
+        return True
+    return False
 
-n = int(input())
-d = set()
-flag = -2
-mn = 10 ** 28
-mx = -10 ** 28
-for _ in range(n):
-    query = input().split()
-    if query[0] == "+":
-        if flag == -2:
-            value = int(query[1])
-        else:
-            value = (flag + int(query[1])) % (10 ** 9)
-        if value not in d:
-            d.add(value)
-            flag = -2
-            mx = max(mx, value)
-            mn = min(mn, value)
+def is_avl_tree(root):
+    if root is None:
+        return True
+    return is_balanced(root) and is_avl_tree(root.left) and is_avl_tree(root.right)
 
-    elif query[0] == "?":
-        value = int(query[1])
-        result = find_min_greater_equal(list(d) )
-        flag = result
-        print(result)
+def is_right_subtree_valid(node):
+    if node is None:
+        return True
+    if node.right and node.right.key <= node.key:
+        return False
+    return is_right_subtree_valid(node.left) and is_right_subtree_valid(node.right)
+
+def is_left_subtree_valid(node):
+    if node is None:
+        return True
+    if node.left and node.left.key >= node.key:
+        return False
+    return is_left_subtree_valid(node.left) and is_left_subtree_valid(node.right)
+
+n, r = map(int, input().split())
+
+nodes = [Node(i) for i in range(n)]
+for i in range(n):
+    li, ri = map(int, input().split())
+    if li != -1:
+        nodes[i].left = nodes[li]
+    if ri != -1:
+        nodes[i].right = nodes[ri]
+
+if is_avl_tree(nodes[r]) and is_right_subtree_valid(nodes[r]) and is_left_subtree_valid(nodes[r]):
+    print(1)
+else:
+    print(0)
--- Занятие 3. Деревья/ДЗ/B.py
+++ Занятие 3. Деревья/ДЗ/B.py
@@ -1,20 +1,68 @@
+from functools import *
+import sys
+
+sys.setrecursionlimit(10 ** 9)
+
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+@lru_cache(None)
 def height(node):
     if node is None:
         return 0
-    else:
-        return max(height(node.left), height(node.right)) + 1
+    return 1 + max(height(node.left), height(node.right))
 
 
+@lru_cache(None)
+def is_balanced(node):
+    if node is None:
+        return True
+    lh = height(node.left)
+    rh = height(node.right)
+    if abs(lh - rh) <= 1 and is_balanced(node.left) and is_balanced(node.right):
+        return True
+    return False
+
+
+@lru_cache(None)
+def is_avl_tree(root):
+    if root is None:
+        return True
+    return is_balanced(root) and is_avl_tree(root.left) and is_avl_tree(root.right)
+
+
+@lru_cache(None)
+def is_right_subtree_valid(node):
+    if node is None:
+        return True
+    if (node.right and node.right.key <= node.key) or (node.left and node.left.key >= node.key):
+        return False
+    return is_right_subtree_valid(node.left) and is_right_subtree_valid(node.right)
+
+
+'''def is_left_subtree_valid(node):
+    if node is None:
+        return True
+    if node.left and node.left.key >= node.key:
+        return False
+    return is_left_subtree_valid(node.left) and is_left_subtree_valid(node.right)'''
 
 n, r = map(int, input().split())
-roots = dict()
 
+nodes = tuple(Node(i) for i in range(n))
 for i in range(n):
-    roots[i] = list(map(int, input().split()))
+    li, ri = map(int, input().split())
+    if li != -1:
+        nodes[i].left = nodes[li]
+    if ri != -1:
+        nodes[i].right = nodes[ri]
 
-print(roots)
-
-flag = 1
-for parent, children in roots.items():
-    if children[0] < parent < children[1]:
-        ...
+if is_avl_tree(nodes[r]) and is_right_subtree_valid(nodes[r]):
+    print(1)
+else:
+    print(0)
